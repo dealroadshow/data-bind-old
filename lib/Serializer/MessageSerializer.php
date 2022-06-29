62a7265fe97af8fa8ec2c7744b8b5f122711566f
Volodymyr Kot <vladimir.kot@finsight.com>
2022-06-29 16:39:59 +0300

    fix of MessageSerializer::serialize return type, FIN-14563

--- lib/Serializer/MessageSerializer.php
+++ lib/Serializer/MessageSerializer.php
@@ -4,9 +4,11 @@ declare(strict_types=1);
 
 namespace Granule\DataBind\Serializer;
 
+use Exception;
 use Google\Protobuf\Internal\Message;
 use Granule\DataBind\Serializer;
 use Granule\DataBind\Type;
+use JsonException;
 
 class MessageSerializer extends Serializer
 {
@@ -15,12 +17,18 @@ class MessageSerializer extends Serializer
         return $type->is(Message::class);
     }
 
-    public function serialize($data): string
+    /**
+     * @throws JsonException
+     */
+    public function serialize($data): ?object
     {
         /** @var Message $datax */
         return json_decode($data->serializeToJsonString(), false, 512, JSON_THROW_ON_ERROR);
     }
 
+    /**
+     * @throws Exception
+     */
     public function unserializeItem($data, Type $type)
     {
         /** @var Message $message */
